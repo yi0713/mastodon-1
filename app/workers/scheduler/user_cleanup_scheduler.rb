d9f0b306728b546a7e2f68f79dd1ed1788f9d8fa
yi0713 <kurage.cc@gmail.com>
2018-01-14 01:10:58 +0900

    User cleanup task references Devise's config variable (User.confirm_within) (Refer theboss.tech)

--- app/workers/scheduler/user_cleanup_scheduler.rb
+++ app/workers/scheduler/user_cleanup_scheduler.rb
@@ -5,7 +5,7 @@ class Scheduler::UserCleanupScheduler
   include Sidekiq::Worker
 
   def perform
-    User.where('confirmed_at is NULL AND confirmation_sent_at <= ?', 2.days.ago).find_in_batches do |batch|
+    User.where('confirmed_at is NULL AND confirmation_sent_at <= ?', User.confirm_within.ago).find_in_batches do |batch|
       Account.where(id: batch.map(&:account_id)).delete_all
       User.where(id: batch.map(&:id)).delete_all
     end
