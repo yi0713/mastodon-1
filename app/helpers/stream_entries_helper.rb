fdbdc1ace9b53f95b6d940d6b4c6f209167fd082
yi0713 <kurage.cc@gmail.com>
2019-05-12 23:02:29 +0900

    Fix bad merge

--- app/helpers/stream_entries_helper.rb
+++ app/helpers/stream_entries_helper.rb
@@ -132,7 +132,18 @@ module StreamEntriesHelper
     end
   end
 
-  def style_classes(status, is_predecessor, is_successor, include_threads)
+  def text_formatting_classes
+    case current_user&.setting_strip_formatting
+    when 'none', nil
+      'rich-text rich-blocks'
+    when 'blocks'
+      'rich-text'
+    when 'all'
+      nil
+    end
+  end
+  
+def style_classes(status, is_predecessor, is_successor, include_threads)
     classes = ['entry']
     classes << 'entry-predecessor' if is_predecessor
     classes << 'entry-reblog' if status.reblog?
