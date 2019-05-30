c39353fa64e45bf71ceee7d3885d1bb9ac703f46
yi0713 <kurage.cc@gmail.com>
2019-05-30 19:08:55 +0900

    Remove source.rb

--- config/initializers/source.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-module Mastodon
-  module Version
-    module_function
-    def source_base_url
-      'https://github.com/yi0713/mastodon/tree/kurage'
-    end
-  end
-end
