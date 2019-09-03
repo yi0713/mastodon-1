bfed707ed46a751799c36d0e428996b090523dcd
yi0713 <kurage.cc@gmail.com>
2019-09-03 20:46:43 +0900

    Fix typo

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -31,11 +31,11 @@ Rails.application.config.content_security_policy do |p|
     webpacker_urls = %w(ws http).map { |protocol| "#{protocol}#{Webpacker.dev_server.https? ? 's' : ''}://#{Webpacker.dev_server.host_with_port}" }
 
     p.connect_src :self, :data, :blob, assets_host, media_host, Rails.configuration.x.streaming_api_base_url, *webpacker_urls
-    p.script_src  :self, :unsafe_inline, :unsafe_eval, assets_host, "https:/    /www.google.com/recaptcha/", "https://www.gstatic.com/recaptcha/"
+    p.script_src  :self, :unsafe_inline, :unsafe_eval, assets_host, "https://www.google.com/recaptcha/", "https://www.gstatic.com/recaptcha/"
     p.worker_src  :self, :blob, assets_host
   else
     p.connect_src :self, :data, :blob, assets_host, media_host, Rails.configuration.x.streaming_api_base_url
-    p.script_src  :self, assets_host, "'sha256-xbJO5dgoFfIDPo/0kyn7FWfZfsJFC    vS/+lpNOzvpAOM='", "https://www.google.com/recaptcha/", "https://www.gstatic    .com/recaptcha/"
+    p.script_src  :self, assets_host, "'sha256-xbJO5dgoFfIDPo/0kyn7FWfZfsJFCvS/+lpNOzvpAOM='", "https://www.google.com/recaptcha/", "https://www.gstatic.com/recaptcha/"
     p.worker_src  :self, :blob, assets_host
   end
 end
